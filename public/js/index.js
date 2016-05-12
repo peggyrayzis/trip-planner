f56d36ce7aa2f82f1023e4e47f1ec08974c2f37c
Peggy Rayzis <peggyrayzis@gmail.com>
2016-05-12 09:42:29 -0400

    Overlays working

---
+++ public/js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	$(".daysselect").overlay({
+		oneInstance: false,
+		closeOnClick: false,
+		top: 10,
+		left: '3%'
+	});
+
+	$(".mapselect").overlay({
+		oneInstance: false,
+		closeOnClick: false,
+		top: 10,
+		left: '65%',
+		right: -10
+	});
+	$(".restaurantselect").overlay({
+		oneInstance: false,
+		closeOnClick: false,
+		top: '65%',
+		left: '65%',
+		right: -10
+	});
+	$(".hotelselect").overlay({
+		oneInstance: false,
+		closeOnClick: false,
+		top: 10,
+		left: '60%',
+		right: -10
+	});
+	$(".activityselect").overlay({
+		oneInstance: false,
+		closeOnClick: false,
+		top: 10,
+		left: '60%',
+		right: -10
+	});
+})
+
